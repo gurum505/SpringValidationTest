001c8670a9d23999f52827162a838b42954307e2
gurum505 <sayhojeong505@gmail.com>
2025-12-01 14:34:51 +0900

    spring validation 강의 19강 html 분리 -16강 @ModelAttribute는 함수에도 가능하다-19강

--- src/main/java/com/back/SpringValidationTest_1/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/SpringValidationTest_1/domain/post/post/controller/PostController.java
@@ -10,68 +10,66 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
 
-    private String getWriteFormHtml() {
-        return getWriteFormHtml("","","","");
-    }
-
-    private String getWriteFormHtml(String errorMessage,String title, String content,String errorFieldName) {
-        return """
-                <div style="color: red;">%s</div>
-                
-                <form method="POST" action="doWrite">
-                  <input type="text" name="title" placeholder="제목" value="%s"autofocus>
-                  <br>
-                  <textarea name="content" placeholder="내용">%s</textarea>
-                  <br>
-                  <input type="submit" value="작성">
-                </form>
-                
-                <script>
-                const errorFieldName = '%s';
-                if ( errorFieldName.length > 0 ){
-                    const forms = document.querySelectorAll('form');
-                    const lastForm = forms[forms.length - 1];
-                    lastForm[errorFieldName].focus();
-                }   
-                </script>
-                
-                """.formatted(errorMessage,title,content,errorFieldName);
+    @ModelAttribute("siteName")
+    public String siteName(){
+        return "커뮤니티사이트A";
     }
 
     @GetMapping("/posts/write")
-    @ResponseBody
-    public String showWrite() {
-        return getWriteFormHtml();
+    public String showWrite(@ModelAttribute("form") WriteForm writeForm) {
+        return "post/post/write";
     }
 
 
     @AllArgsConstructor
     @Getter
     public static class WriteForm {
-        @NotBlank
-        @Size(min = 2, max = 20)
+        @NotBlank(message = "1-제목을 입력해주세요.")
+        @Size(min = 2, max = 20, message = "2-제목은 2자 이상, 20자 이하로 입력가능합니다.")
         private String title;
-        @NotBlank
-        @Size(min = 2, max = 20)
+        @NotBlank(message = "3-내용을 입력해주세요.")
+        @Size(min = 2, max = 20, message = "4-내용은 2자 이상, 20자 이하로 입력가능합니다.")
         private String content;
     }
 
     @PostMapping("/posts/doWrite")
-    @ResponseBody
     @Transactional
     public String write(
-            @Valid WriteForm form
+            @ModelAttribute("form") @Valid WriteForm form,
+            BindingResult bindingResult,
+            Model model
     ) {
-        Post post = postService.write(form.getTitle(),form.getContent());
+        if (bindingResult.hasErrors()) {
 
-        return "%d번 글이 생성되었습니다.".formatted(post.getId());
+            String errorMessage = bindingResult
+                    .getFieldErrors()
+                    .stream()
+                    .map(fieldError -> (fieldError.getField() + "-" + fieldError.getDefaultMessage()).split("-", 3))
+                    .map(field -> "<!--%s--><li data-error-field-name=\"%s\">%s</li>".formatted(field[1], field[0], field[2]))
+                    .sorted()
+                    .collect(Collectors.joining("\n"));
+            model.addAttribute("errorMessage",errorMessage);
+            return "post/post/write";
+        }
+
+        Post post = postService.write(form.getTitle(), form.getContent());
+
+        model.addAttribute("post", post);
+
+        return "post/post/writeDone";
     }
 }
